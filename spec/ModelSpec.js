d76888de2927c2ce3ef8b741cd21991edcd0467d
Daniel St. Jules <danielst.jules@gmail.com>
2013-12-16 23:14:01 -0500

    Alternative async jasmine spec style

--- spec/ModelSpec.js
+++ spec/ModelSpec.js
@@ -10,27 +10,22 @@ describe("Model utility functions", function() {
 		var seq1 = Model.sequence();
 		expect(seq1()).toBe("0");
 		expect(seq1()).toBe("1");
+
 		var seq2 = Model.sequence();
 		expect(seq2()).toBe("0");
 		expect(seq2()).toBe("1");
 		expect(seq1()).toBe("2");
 	});
+
 	it("generate UUIDs by default", function() {
 		expect(typeof(TestModel.idgen())).toBe("string");
 		expect(TestModel.idgen()).toMatch(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i);
 	});
 });
 
-describe("Model class", function() {
-	beforeEach(function() {
-		var purged = false;
-		runs(function() {
-			TestModel.purge(function() { purged = true; });
-		});
-
-		waitsFor(function() {
-			return purged;
-		});
+describe("Model class", function(done) {
+	beforeEach(function(done) {
+		TestModel.purge(done);
 	});
 
 	it("has expected static methods", function() {
@@ -45,139 +40,79 @@ describe("Model class", function() {
 		expect(typeof(test.toObject)).toBe("function");
 	});
 
-	it("(C)reates objects as redis hashes", function() {
-		var saved = false, test, obj;
-		runs(function() {
-			test = new TestModel();
-			test.foo = "test one";
-			test.bar = 234;
-			test.save(function(err, res) {
-				if (res) saved = true;
-			});
-		});
+	it("(C)reates objects as redis hashes", function(done) {
+		var test = new TestModel();
+		test.foo = "test one";
+		test.bar = 234;
+		test.save(function(err, res) {
+			if (err) done(err);
 
-		waitsFor(function() { return saved; }, "Object to be saved", 500);
-
-		runs(function() {
 			client.hgetall(TestModel.getKey(test.id), function(err, res) {
-				obj = res;
+				if (err) done(err);
+				expect(res.foo).toBe(test.foo);
+				expect(res.bar).toBe(new String(test.bar).valueOf());
+				expect(res).not.toBe(test);
+				done();
 			});
-		});
-
-		waitsFor(function() { return obj !== undefined; }, "Object to be populated", 500);
-
-		runs(function() {
-			expect(obj.foo).toBe(test.foo);
-			expect(obj.bar).toBe(new String(test.bar).valueOf());
-			expect(obj).not.toBe(test);
 		});
 	});
 
-	it("(R)etrieves saved objects", function() {
-		var saved = false, copy, test;
-		runs(function() {
-			test = new TestModel();
-			test.foo = "test one two three";
-			test.bar = 23456;
-			test.save(function() { saved = true; });
-		});
+	it("(R)etrieves saved objects", function(done) {
+		var test = new TestModel();
+		test.foo = "test one two three";
+		test.bar = 23456;
+		test.save(function(err) {
+			if (err) done(err);
 
-		waitsFor(function() {
-			return saved;
-		}, "The object to be saved", 500);
-
-		runs(function() {
 			TestModel.get(test.id, function(err, res) {
-				copy = res;
+				if (err) done(err);
+				expect(res.id).toBe(test.id);
+				expect(res.foo).toBe(test.foo);
+				expect(res.bar).toBe(new String(test.bar).valueOf());
+				done();
 			});
-		});
-
-		waitsFor(function() {
-			return (copy !== undefined);
-		}, "The copy to be returned", 500);
-
-		runs(function() {
-			expect(copy.id).toBe(test.id);
-			expect(copy.foo).toBe(test.foo);
-			expect(copy.bar).toBe(new String(test.bar).valueOf());			
 		});
 	});
 
-	it("(U)pdates objects", function() {
-		var saved = false, copy, test;
-		runs(function() {
-			test = new TestModel();
-			test.foo = "First save";
-			test.save(function() { saved = true; });
-		});
+	it("(U)pdates objects", function(done) {
+		var test = new TestModel();
+		test.foo = "First save";
+		test.save(function(err) {
+			if (err) done(err);
 
-		waitsFor(function() {
-			return saved;
-		}, "The object to be saved", 500);
-
-		runs(function() {
-			saved = false;
 			test.foo = "Updated value";
-			test.save(function() { saved = true});
-		});
+			test.save(function(err) {
+				if (err) done(err);
 
-		waitsFor(function() {
-			return saved;
-		}, "The object to be updated", 500);
-
-		runs(function() {
-			TestModel.get(test.id, function(err, res) {
-				copy = res;
+				TestModel.get(test.id, function(err, res) {
+					if (err) done(err);
+					expect(res.id).toBe(test.id);
+					expect(res.foo).toBe(test.foo);
+					done();
+				});
 			});
-		});
-
-		waitsFor(function() {
-			return (copy !== undefined);
-		}, "The copy to be retrieved", 500);
-
-		runs(function() {
-			expect(copy.id).toBe(test.id);
-			expect(copy.foo).toBe(test.foo);
 		});
 	});
 
-	it("(D)eletes objects", function() {
-		var saved = false, test, result;
-		runs(function() {
-			test = new TestModel();
-			test.save(function() { saved = true; });
-		});
+	it("(D)eletes objects", function(done) {
+		test = new TestModel();
+		test.save(function(err) {
+			if (err) done(err);
 
-		waitsFor(function() {
-			return saved;
-		}, "Object to be saved", 500);
+			test.delete(function(err) {
+				if (err) done(err);
+				var m = client.multi();
+				var k = TestModel.getKey(test.id);
+				m.hgetall(k);
+				m.sismember(TestModel.getKey('all'), k);
 
-		runs(function() {
-			saved = false;
-			test.delete(function() { saved = true; });
-		});
-
-		waitsFor(function() {
-			return saved;
-		}, "Object to be deleted", 500);
-
-		runs(function() {
-			var m = client.multi();
-			var k = TestModel.getKey(test.id);
-			m.hgetall(k);
-			m.sismember(TestModel.getKey('all'), k);
-			m.exec(function(err, res) {
-				result = res;
+				m.exec(function(err, res) {
+					if (err) done(err);
+					expect(res[0]).toEqual(null);
+					expect(res[1]).toBe(0);
+					done();
+				});
 			});
-		});
-
-		waitsFor(function() {
-			return result !== undefined;
-		}, "Data comes back from Redis", 500);
-
-		runs(function() {
-			expect(result[0]).toEqual(null);
-			expect(result[1]).toBe(0);
 		});
 	});
 
