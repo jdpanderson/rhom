1c874fd7215b426bc0a8474267199bdb017bd19d
Jonathan Anderson <janderson@janderson.ca>
2013-12-17 22:39:16 -0500

    WIP relations. Just dumped some code into the editor. No testing, no spec. Not usable.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-/**
- * For now, only export the main model and cache. Plan is to add other mixins later.
- */
+/* Main mixin augments classes with CRUD */
 module.exports = require('./lib/Model.js');
+
+/* Cache mixin adds transparent caching on top of standard CRUD. */
 module.exports.cache = require('./lib/Cache.js');
+
+/* Relations mixin adds object relationships. */
+module.exports.relates = require('./lib/Relation.js');
 
 // ... Future? Other ideas?
 //module.exports.eventemitter = require('./EventEmitter.js');
---
+++ spec/RelationSpec.js
@@ -0,0 +1,5 @@
+var client = require('fakeredis').createClient(null, null, {fast: true});
+var Model = require('../lib/Model.js');
+var Relation = require('../lib/Relation.js');
+
+
