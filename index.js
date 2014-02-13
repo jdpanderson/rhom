b4b4668882aa8741a5e664c0cd7ae6dc4d7b6071
Jonathan Anderson <janderson@janderson.ca>
2014-02-12 20:24:02 -0500

    WIP Index functionality.

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ module.exports.cache = require('./lib/Cache.js');
 /* Relations mixin adds object relationships. */
 module.exports.relates = require('./lib/Relation.js');
 
+/* Index a field by value. */
+// Works, but not ready. See Index.js for info.
+module.exports.index = require('./lib/Index.js');
+
 // Placeholder for future ideas. See bottom of README.md for info.
 //module.exports.eventemitter = require('./EventEmitter.js');
 //module.exports.index = require('./Index.js');
